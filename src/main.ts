8a7f1ae5835c1989f545b29ae9d67473beac9c8c
77-ahmad-collab <ahmaq098@gmail.com>
2022-01-07 10:40:12 +0500

    content added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+require('dotenv').config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
+  const app = await NestFactory.create(AppModule, { cors: true });
   console.log('app is listening on port 3000');
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
