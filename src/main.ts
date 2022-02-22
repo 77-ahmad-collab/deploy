626935256010c000633ae52cd212695a33b59b2f
77-ahmad-collab <ahmaq098@gmail.com>
2022-02-23 02:05:49 +0500

    Added

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
+  const PORT = process.env.PORT || 5000;
   console.log('app is listening on port 5000');
-  await app.listen(5000);
+  console.log(process.env.PORT);
+  await app.listen(PORT);
 }
 bootstrap();
